b7fae23d7be0e8de56c6351f98eda2bf1abf67dc
Ojas Chaturvedi <oj.chaturvedi.2024@gmail.com>
2024-10-26 22:44:35 -0400

    Format js file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 // src/App.js
-import React, { useState } from 'react';
-import MapComponent from './components/MapComponent';
-import SidebarContainer from './components/SidebarContainer';
-import spots from './components/MarkerInfo.json';
-import './styles/primarySidebar.css';
-import './styles/detailedSidebar.css';
+import React, { useState } from "react";
+import MapComponent from "./components/MapComponent";
+import SidebarContainer from "./components/SidebarContainer";
+import spots from "./components/MarkerInfo.json";
+import "./styles/primarySidebar.css";
+import "./styles/detailedSidebar.css";
 
 function App() {
-    const [highlightedSpotId, setHighlightedSpotId] = useState(null);
+	const [highlightedSpotId, setHighlightedSpotId] = useState(null);
 
-    return (
-        <div className="App">
-            <MapComponent
-                highlightedSpotId={highlightedSpotId}
-                setHighlightedSpotId={setHighlightedSpotId} // Pass setter directly
-            />
-            <SidebarContainer
-                spots={spots}
-                highlightedSpotId={highlightedSpotId}
-                setHighlightedSpotId={setHighlightedSpotId} // Pass setter directly
-            />
-        </div>
-    );
+	return (
+		<div className="App">
+			<MapComponent
+				highlightedSpotId={highlightedSpotId}
+				setHighlightedSpotId={setHighlightedSpotId} // Pass setter directly
+			/>
+			<SidebarContainer
+				spots={spots}
+				highlightedSpotId={highlightedSpotId}
+				setHighlightedSpotId={setHighlightedSpotId} // Pass setter directly
+			/>
+		</div>
+	);
 }
 
 export default App;
